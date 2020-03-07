25ed8bd23fd21d81647c305b6fe30132ae29b57b
Sahithyan <sahithyan2701@gmail.com>
2020-03-07 19:27:54 +0530

    First commit

---
+++ 03.03.2020 - Matlida's cupcakes/app.js
@@ -0,0 +1,57 @@
+const fs = require('fs')
+
+function getWeeklyTotals() {
+    let totalMoneyMade = fs.readFileSync('./Total.txt').toString().split('\n').slice(1).reverse()
+    let weeklyTotals = []
+    let lastWeekTotal = 0
+
+    for (let i in totalMoneyMade) {
+        let todayTotal = totalMoneyMade[i]
+
+        if (i % 7 == 0 && i != 0) {
+            weeklyTotals.push(lastWeekTotal)
+            lastWeekTotal = 0
+        }
+        lastWeekTotal += +todayTotal
+    }
+    if (lastWeekTotal != 0) weeklyTotals.push(lastWeekTotal)
+    return weeklyTotals
+}
+
+function getMonthlyTotals() {
+    let monthlyTotals = []
+    let weeklyTotals = getWeeklyTotals()
+    let lastMonthTotal = 0
+
+    for (let i in weeklyTotals) {
+        let thisWeekTotal = weeklyTotals[i]
+        if (i % 4 == 0 && i != 0) {
+            monthlyTotals.push(lastMonthTotal)
+            lastMonthTotal = 0
+        }
+        lastMonthTotal += thisWeekTotal
+    }
+    if (lastMonthTotal != 0) monthlyTotals.push(lastMonthTotal)
+    return monthlyTotals
+}
+
+function getYearlyReport() {
+    let yearlyTotals = []
+    let monthlyTotals = getMonthlyTotals()
+    let lastYearTotal = 0
+
+    for (let i in monthlyTotals) {
+        let thisMonthTotal = monthlyTotals[i]
+        if (i % 12 == 0 && i != 0) {
+            yearlyTotals.push(lastYearTotal)
+            lastYearTotal = 0
+        }
+        lastYearTotal += thisMonthTotal
+
+    }
+    if (lastYearTotal != 0) yearlyTotals.push(lastYearTotal)
+
+    return yearlyTotals
+}
+
+console.log(getYearlyReport())
